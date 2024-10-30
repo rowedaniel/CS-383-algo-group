b0c052fd06281f7f6f622992f91c30f16d0ee938
grorthory <alexskiles123@gmail.com>
2024-10-30 12:26:49 -0700

    brute passing 4/5

--- src/brute_force_match.py
+++ src/brute_force_match.py
@@ -1,24 +1,32 @@
 
 def match(pattern, text):
-    print(len(text))
 
-    for i in range (len(text) - len(pattern)):
+    for i in range (len(text) - len(pattern)+1):
 
-        for j in range(len(text) - len(pattern)):
+        for j in range(len(text) - len(pattern)+1):
 
+            match_is_good = True
             for k in range(len(pattern)):
 
                 for l in range(len(pattern)):
 
                     print(pattern[k][l])
-                
+                    if (pattern[k][l] != text[i+k][j+l]):
+                        match_is_good = False
+                        break
+                if not match_is_good:
+                    break
+            if match_is_good:
+                return i,j
+    return None
+
 
 
 if __name__ == "__main__":
-    match([[0]],
-        [[0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 2]]
+    m = match([[1,1],[1,1]],
+        [[0, 1, 1, 1],
+        [0, 1, 1, 1],
+        [1, 1, 1, 1],
+        [0, 1, 0, 1]]
     )
 
