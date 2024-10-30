57d65c69e11f70f87926e38425ba198ff776f54b
Elliot Mayer <elliot.w.mayer@gmail.com>
2024-10-30 12:06:08 -0700

    basic outline

--- src/brute_force_match.py
+++ src/brute_force_match.py
@@ -1,11 +1,24 @@
-class match():
-    def __init__(self) -> None:
-        pass
-        
-    def match(self, pattern, text):
-        self.pattern = pattern
-        self.text = text
 
-        for i in range (len(self.text)):
+def match(pattern, text):
+    print(len(text))
 
-            for j in range (len(self.text) - len(self.pattern))
+    for i in range (len(text) - len(pattern)):
+
+        for j in range(len(text) - len(pattern)):
+
+            for k in range(len(pattern)):
+
+                for l in range(len(pattern)):
+
+                    print(pattern[k][l])
+                
+
+
+if __name__ == "__main__":
+    match([[0]],
+        [[0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 2]]
+    )
+
