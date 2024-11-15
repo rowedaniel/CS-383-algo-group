16114d3d9d38b0d0fda503164a15237897c51f70
Elliot Mayer <elliot.w.mayer@gmail.com>
2024-11-15 12:02:58 -0800

    added gerrymander

--- src/brute_force_match.py
+++ src/brute_force_match.py
@@ -1,16 +1,17 @@
 
 def match(pattern, text):
-
-    for i in range (len(text) - len(pattern)+1):
-
-        for j in range(len(text) - len(pattern)+1):
-
+    count = 0
+    for i in range ((len(text) - len(pattern))+1):
+        
+        for j in range((len(text) - len(pattern))+1):
+            
             match_is_good = True
             for k in range(len(pattern)):
-
+                
                 for l in range(len(pattern)):
+                    count += 1
+                    
 
-                    print(pattern[k][l])
                     if (pattern[k][l] != text[i+k][j+l]):
                         match_is_good = False
                         break
@@ -23,10 +24,13 @@ def match(pattern, text):
 
 
 if __name__ == "__main__":
-    m = match([[1,1],[1,1]],
-        [[0, 1, 1, 1],
-        [0, 1, 1, 1],
-        [1, 1, 1, 1],
-        [0, 1, 0, 1]]
+    m = match([[4,4],
+               [4,4]],
+
+        [[4, 4, 0, 1],
+        [3, 4, 4, 1],
+        [1, 4, 4, 4],
+        [0, 1, 4, 4]]
     )
+    print(m)
 
---
+++ src/fingerprint_match.py
@@ -0,0 +1,39 @@
+
+def match(pattern, text):
+    count = 0
+    for i in range ((len(text) - len(pattern))+1):
+        
+        for j in range((len(text) - len(pattern))+1):
+            
+            match_is_good = True
+            for k in range(len(pattern)):
+                
+                for l in range(len(pattern)):
+                    count += 1
+                    print("i and j: {} {}".format(i, j))
+                    print("k and l: {} {}".format(k, l))
+                    print("______")
+                    print ("count: {}".format(count))
+
+                    if (pattern[k][l] != text[i+k][j+l]):
+                        match_is_good = False
+                        break
+                if not match_is_good:
+                    break
+            if match_is_good:
+                return i,j
+    return None
+
+
+
+if __name__ == "__main__":
+    m = match([[4,4],
+               [4,4]],
+
+        [[4, 4, 0, 1],
+        [3, 4, 4, 1],
+        [1, 4, 4, 4],
+        [0, 1, 4, 4]]
+    )
+    print(m)
+
